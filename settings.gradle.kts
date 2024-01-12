f04e0654a39fac3dea85d09a688ce2f4d139a30c
Paolo Penazzi <58599380+PaoloPenazzi@users.noreply.github.com>
2024-01-12 10:40:45 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.gradle.enterprise") version "3.16.1"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
+
+rootProject.name = "Template-for-Kotlin-JVM-Projects"
